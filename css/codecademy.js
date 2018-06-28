848ef59b204701292d5f39e2e093e0606df192a2
Biodun2 <agoyonamandine@gmail.com>
2018-06-28 16:43:00 +0100

    first commit

---
+++ css/codecademy.js
@@ -0,0 +1,31 @@
+const restaurant = {
+  name: 'Italian Bistro',
+  seatingCapacity: 120,
+  hasDineInSpecial: true,
+  entrees: ['Penne alla Bolognese', 'Chicken Cacciatore', 'Linguine pesto'],
+  openRestaurant: function() {
+    if (this.hasDineInSpecial) {
+      return 'Unlock the door, post the special on the board, then flip the open sign.'
+    } else {
+      return 'Unlock the door, then flip the open sign.'
+    }
+  }
+}
+
+console.log(restaurant.openRestaurant());
+
+const restaurant = {
+  name: 'Italian Bistro',
+  seatingCapacity: 120,
+  hasDineInSpecial: true,
+  entrees: ['Penne alla Bolognese', 'Chicken Cacciatore', 'Linguine pesto'],
+  openRestaurant() {
+    if (hasDineInSpecial) {
+      return 'Unlock the door, post the special on the board, then flip the open sign.';
+    } else {
+      return 'Unlock the door, then flip the open sign.';
+    }
+  }
+};
+
+console.log(restaurant.openRestaurant());
